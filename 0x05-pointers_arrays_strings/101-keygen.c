157dcb168284f9472c99cafceb6685da177c04ac
Aphiweg1 <aphiweg1@gmail.com>
2023-07-24 16:16:21 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -28,7 +28,7 @@ if (sum != 2772)
 {
 diff_half1 = (sum - 2772) / 2;
 diff_half2 = (sum - 2772) / 2;
-if ((sum - 2772) % 2 != 0)						
+if ((sum - 2772) % 2 != 0)				
 for (index = 0; password[index]; index++)
 password[index] -= diff_half1;
 }
