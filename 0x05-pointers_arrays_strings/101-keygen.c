b110f2d7bd44c28630a2be02bb67bce4ff4a15a6
Aphiweg1 <aphiweg1@gmail.com>
2023-07-24 17:26:28 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,24 +9,20 @@
  */
 int main(void)
 {
-int password[64], i, sum = 0, n;
+char password[16];
+int i, sum = 0;
 srand(time(NULL));
 
-for (i = 0; i < 64; i++)
+for (i = 0; i < 15; i++)
 {
-password[i] = rand() % 78;
-sum += password[i] + '0';
-putchar(password[i] + '0');
-
-if ((2772 - sum) < 78)
-{
-n = 2772 - sum;
-sum += n;
-putchar(n + '0');
+password[i] = rand() % 94 + 33;
+putchar(password[i]);
+sum += password[i];
+}
+password[15] = 2772 - sum;
+putchar(password[15]);
+password[16] = '\0';
 printf("\nTada! Congrats\n");
-break;
-}
-}
 
-return 0;
+return (0);
 }
